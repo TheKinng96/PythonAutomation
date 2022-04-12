f4ae5e2fe590d417335af78052547465de80b4e6
Your name <Your email>
2022-04-12 22:08:02 +0900

    edited automation for testing

--- automation.py
+++ automation.py
@@ -1,23 +1,55 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+from model import user
 
-chrome_browser = webdriver.Chrome('./chromedriver.exe')
+# Defined driver and file
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+f = open('./text.txt', 'w')
 
-chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+def log(message):
+  f.write(message)
+  f.write('\n')
 
-# print('Selenium Easy Demo' in chrome_browser.title)
-# asset 'Selenium Easy Demo' in chrome_browser.title
+driver.get(user["website"])
+assert "アドシスト" in driver.title
+message = 'Accessed website: ' + driver.title + ' at ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+log(message)
 
-button = chrome_browser.find_element_by_class_name('btn-default')
-# print(button_text.get_attribute('innerHTML'))
+# Login
+emailField = driver.find_element(By.ID, value='loginEmail')
+emailField.clear()
+emailField.send_keys(user["email"])
+passwordField = driver.find_element(By.ID, value='loginPassword')
+passwordField.clear()
+passwordField.send_keys(user["password"])
+buttonLogin = driver.find_element(By.ID, value='buttonLogin')
+buttonLogin.send_keys(Keys.RETURN)
+message = "Logined successfully as " + user["email"]
+log(message)
 
-assert 'Show Message' in chrome_browser.page_source
+# emailField = driver.find_element(By.ID, 'loginEmail')
+# emailField.send_keys('wordpress-ec@test.com')
+# passwordField = driver.find_element('loginPassword')
+# passwordField.send_keys('password')
 
-message_input = chrome_browser.find_element_by_id("user-message")
-message_input.clear()
-message_input.send_keys('Hey yo~')
-button.click()
+# button = driver.find_element_by_class_name('btn-default')
+# # print(button_text.get_attribute('innerHTML'))
 
-output_message = chrome_browser.find_element_by_id('display')
-assert 'Hey yo~' in output_message.text
+# assert 'Show Message' in driver.page_source
 
-chrome_browser.close()
+# message_input = driver.find_element_by_id("user-message")
+# message_input.clear()
+# message_input.send_keys('Hey yo~')
+# button.click()
+
+# output_message = driver.find_element_by_id('display')
+# assert 'Hey yo~' in output_message.text
+
+# driver.close()
